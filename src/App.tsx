fa582a605194d7e2792e6176a22428627ac42169
amanzrx4 <amansagar262@gmail.com>
2023-05-29 11:20:13 +0530

    add alert in place of log

--- src/App.tsx
+++ src/App.tsx
@@ -10,10 +10,10 @@ function App() {
 		customProtocolCheck(
 			APP_URI,
 			() => {
-				console.log('Custom protocol not found.')
+				alert('Custom protocol not found.')
 			},
 			() => {
-				console.log('Custom protocol found and opened the file successfully.')
+				alert('Custom protocol found and opened the file successfully.')
 			},
 			5000
 		)
