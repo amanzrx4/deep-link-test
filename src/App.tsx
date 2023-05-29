a3cfe86229871096e38ec681bf94e1abbbb29c44
amanzrx4 <amansagar262@gmail.com>
2023-05-29 15:52:11 +0530

    chrome intent

--- src/App.tsx
+++ src/App.tsx
@@ -1,55 +1,75 @@
 import './App.css'
 
-const APP_URI = 'reclaim://template'
+const APP_URI = 'https://www.google.com'
+
+function openReclaimApp() {
+	const uri = 'reclaim://template'
+	const packageName = 'com.credentialswallet'
+
+	const fallbackMarketUri = `market://details?id=${packageName}`
+	const fallbackWebUri = `https://play.google.com/store/apps/details?id=${packageName}`
+
+	if (navigator.userAgent.match(/Chrome/i)) {
+		window.location.href = uri
+	} else if (navigator.userAgent.match(/Android/i)) {
+		window.location.href = fallbackMarketUri
+	} else {
+		window.location.href = fallbackWebUri
+	}
+}
 
 function App() {
 	const handleOpenDeepLink = () => {
-		function checkCustomAppAvailability(protocol: string): void {
-			const iframe = document.createElement('iframe')
-			iframe.style.display = 'none'
+		openReclaimApp()
 
-			const handleAppAvailability = (isAvailable: boolean) => {
-				document.body.removeChild(iframe)
-				handleAppAvailabilityCheck(isAvailable)
-			}
+		// const handleAppAvailabilityCheck = (isAvailable: boolean) => {
+		// 	if (isAvailable) {
+		// 		alert('App exists')
+		// 		// Perform actions when the app exists
+		// 	} else {
+		// 		alert('App does not exists')
+		// 		// Perform actions when the app does not exist
+		// 	}
+		// }
 
-			iframe.onload = () => {
-				handleAppAvailability(true) // Protocol was successfully opened (app exists)
-			}
+		// function checkCustomAppAvailability(protocol: string): void {
+		// 	const iframe = document.createElement('iframe')
+		// 	iframe.style.display = 'none'
 
-			iframe.onerror = () => {
-				handleAppAvailability(false) // Error occurred (app does not exist)
-			}
+		// 	const handleAppAvailability = (isAvailable: boolean) => {
+		// 		document.body.removeChild(iframe)
+		// 		handleAppAvailabilityCheck(isAvailable)
+		// 	}
 
-			document.body.appendChild(iframe)
-			iframe.src = protocol
-		}
+		// 	iframe.onload = () => {
+		// 		console.log('loading')
+		// 		handleAppAvailability(true) // Protocol was successfully opened (app exists)
+		// 	}
 
-		// Usage example
-		checkCustomAppAvailability(APP_URI)
+		// 	iframe.onerror = () => {
+		// 		handleAppAvailability(false) // Error occurred (app does not exist)
+		// 	}
 
-		// Callback function to handle the result
-		const handleAppAvailabilityCheck = (isAvailable: boolean) => {
-			if (isAvailable) {
-				alert('App exists')
-				// Perform actions when the app exists
-			} else {
-				alert('App does not exists')
-				// Perform actions when the app does not exist
-			}
-		}
+		// 	document.body.appendChild(iframe)
+		// 	iframe.src = protocol
+		// }
 
-		// window.location.href = 'reclaim://template'
-		// customProtocolCheck(
-		// 	APP_URI,
-		// 	() => {
-		// 		alert('Custom protocol not found.')
-		// 	},
-		// 	() => {
-		// 		alert('Custom protocol found and opened the file successfully.')
-		// 	},
-		// 	200
-		// )
+		// // Usage example
+		// checkCustomAppAvailability(APP_URI)
+
+		// // Callback function to handle the result
+
+		// // window.location.href = 'reclaim://template'
+		// // customProtocolCheck(
+		// // 	APP_URI,
+		// // 	() => {
+		// // 		alert('Custom protocol not found.')
+		// // 	},
+		// // 	() => {
+		// // 		alert('Custom protocol found and opened the file successfully.')
+		// // 	},
+		// // 	200
+		// // )
 	}
 
 	return (
