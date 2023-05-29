c0846d300720a9d0e9e9297430c57fb4c98121c5
amanzrx4 <amansagar262@gmail.com>
2023-05-29 15:34:31 +0530

    check via iframe

--- src/App.tsx
+++ src/App.tsx
@@ -1,22 +1,55 @@
 import './App.css'
 
-import customProtocolCheck from 'custom-protocol-check'
-
 const APP_URI = 'reclaim://template'
 
 function App() {
 	const handleOpenDeepLink = () => {
+		function checkCustomAppAvailability(protocol: string): void {
+			const iframe = document.createElement('iframe')
+			iframe.style.display = 'none'
+
+			const handleAppAvailability = (isAvailable: boolean) => {
+				document.body.removeChild(iframe)
+				handleAppAvailabilityCheck(isAvailable)
+			}
+
+			iframe.onload = () => {
+				handleAppAvailability(true) // Protocol was successfully opened (app exists)
+			}
+
+			iframe.onerror = () => {
+				handleAppAvailability(false) // Error occurred (app does not exist)
+			}
+
+			document.body.appendChild(iframe)
+			iframe.src = protocol
+		}
+
+		// Usage example
+		checkCustomAppAvailability(APP_URI)
+
+		// Callback function to handle the result
+		const handleAppAvailabilityCheck = (isAvailable: boolean) => {
+			if (isAvailable) {
+				alert('App exists')
+				// Perform actions when the app exists
+			} else {
+				alert('App does not exists')
+				// Perform actions when the app does not exist
+			}
+		}
+
 		// window.location.href = 'reclaim://template'
-		customProtocolCheck(
-			APP_URI,
-			() => {
-				alert('Custom protocol not found.')
-			},
-			() => {
-				alert('Custom protocol found and opened the file successfully.')
-			},
-			200
-		)
+		// customProtocolCheck(
+		// 	APP_URI,
+		// 	() => {
+		// 		alert('Custom protocol not found.')
+		// 	},
+		// 	() => {
+		// 		alert('Custom protocol found and opened the file successfully.')
+		// 	},
+		// 	200
+		// )
 	}
 
 	return (
